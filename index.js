e5a7c18c9b35e51820f2d28b3a2f0ce4674422d6
PremPinreddy <pinreddypremchandra046@gmail.com>
2023-08-27 20:59:40 +0530

    day1

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express=require("express");
 const app=express();
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+const helmet=require("helmet");
+const morgan=require("morgan");
+const userRoute=require("./routes/users")
+const authRoute=require("./routes/auth")
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('connnection successful');
+}).catch((err)=>console.log('no connection'));
+  
+//middle ware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
 
 app.listen(8800,()=>{
     console.log("Backend server is Running")
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const User = require("../models/User")
+const router=require("express").Router();
+
+//update user
+//delete user
+//get a user
+//follow a user
+//unfollow a user 
+
+module.exports=router
